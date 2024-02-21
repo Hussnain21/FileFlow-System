c80f69a7ff00d653f6b1b28e1564bc38ff551b4c
Hussnain Majid <615376@sandwellcollege.ac.uk>
2024-02-21 13:24:39 +0000

    Download file feature

--- src/components/DashboardComp/FileComp/Header.jsx
+++ src/components/DashboardComp/FileComp/Header.jsx
@@ -1,12 +1,36 @@
 import { faAngleLeft, faSave } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useDispatch } from "react-redux";
+import { shallowEqual, useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { updateFileData } from "../../../redux/actionCreators/elementsActionCreator";
 
 const Header = ({ fileName, fileId, fileData, prevFileData }) => {
   const navigate = useNavigate();
   const disptach = useDispatch();
+
+  const { openedFile, isAuthenticated } = useSelector(
+    (state) => ({
+      openedFile: state.elements.userFiles.find(
+        (file) => file.docId === fileId
+      ),
+      isAuthenticated: state.auth.isAuthenticated,
+    }),
+    shallowEqual
+  );
+
+  const downloadFile = () => {
+    const bit = document.createElement("a");
+    bit.setAttribute("href", openedFile.data.data);
+    bit.setAttribute("download", openedFile.data.name);
+    bit.setAttribute("target", "_blank");
+    bit.style.display = "none";
+    document.body.appendChild(bit);
+    bit.click();
+    document.body.removeChild(bit);
+
+    console.log(bit);
+  };
+
   return (
     <nav className="navbar navbar-expand-lg mt-2 navbar-light bg-white shadow-sm">
       <p className="navba-brand my-0 fw-bold ms-5 ">{fileName}</p>
@@ -29,6 +53,11 @@ const Header = ({ fileName, fileId, fileData, prevFileData }) => {
         <li className="nav-item mx-2">
           <button className="btn btn-dark" onClick={() => navigate("/")}>
             <FontAwesomeIcon icon={faAngleLeft} /> Go Back
+          </button>
+        </li>
+        <li className="nav-item mx-2">
+          <button className="btn btn-dark" onClick={() => downloadFile()}>
+            <FontAwesomeIcon icon={faAngleLeft} /> Download
           </button>
         </li>
       </ul>
